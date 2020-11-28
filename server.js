a8896cf58fe33aad5a44acb41115ce6809bd1493
brian otweyo <brianotweyo@gmail.com>
2020-11-28 18:20:49 +0300

    touring

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+// setting the middleware
+app.use(express.static('public'));
+// giving the port a number
+var port = process.env.PORT || 5030;
+
+var server = app.listen(port, () => {
+    console.log(`Server started on port `+ port);
+});
